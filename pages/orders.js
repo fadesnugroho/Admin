64f8305e6d0cb4aa3fb78fbd8132a87988f15a91
fadesnugroho <fadesnugroho@students.amikom.ac.id>
2023-07-24 23:41:13 +0700

    bebas

---
+++ pages/orders.js
@@ -0,0 +1,46 @@
+import Layout from "@/components/Layout";
+import axios from "axios";
+import { useEffect, useState } from "react";
+
+export default function OrdersPage(){
+    const [orders,setOrders] = useState([]);
+    useEffect(() => {
+        axios.get('/api/orders').then(respponse => {
+            setOrders(respponse.data);
+        });
+    }, []);
+    return (
+        <Layout>
+            <h1>Orders</h1>
+            <table className="basic">
+                <thead>
+                    <tr>
+                        <th>Date</th>
+                        <th>Recipient</th>
+                        <th>Products</th>
+                    </tr>
+                </thead>
+                <tbody>
+                {orders.length > 0 && orders.map(order => (
+                    <tr>
+                        <td>{(new Date(order.createdAt)).toLocaleString()}</td>
+                        <td>
+                            {order.name} {order.email}<br />
+                            {order.city} {order.postalCode}<br />
+                            {order.streetAddress}
+                        </td>
+                        <td>
+                          {order.line_items.map(l => (
+                            <>
+                                {l.price_data?.product_data.name} x 
+                                {l.quantity}<br/>
+                            </>
+                          ))}  
+                        </td>
+                    </tr>
+                ))}
+                </tbody>
+            </table>
+        </Layout>
+    )
+}
